1322f80536ff077ab87e5176a13ea5db242254b1
Juliano Donini <julianodonini89@gmail.com>
2018-12-02 13:42:45 -0200

    update clean projecy

--- utils/clean_project.sh
+++ utils/clean_project.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find ~/Workspace/Stacking-Audio-Tagging/ -type d -name "*.pyc" -exec rm {} \;
-find ~/Workspace/Stacking-Audio-Tagging/ -type d -name "*.DS_Store" -exec rm {} \;
-find ~/Workspace/Stacking-Audio-Tagging/ -type d -name "*__pycache__" -exec rm -rf {} \;
+find ~/Workspace/Cats-and-Dogs-Classification/ -type d -name "*.pyc" -exec rm {} \;
+find ~/Workspace/Cats-and-Dogs-Classification/ -type d -name "*.DS_Store" -exec rm {} \;
+find ~/Workspace/Cats-and-Dogs-Classification/ -type d -name "*__pycache__" -exec rm -rf {} \;
