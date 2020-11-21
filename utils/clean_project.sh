d01f99ddb8be7be93baf8c37b35e702404ae0b9f
Juliano Donini <julianodonini89@gmail.com>
2020-11-20 23:24:49 -0300

    Starting updating of older packages and creating new structures

--- utils/clean_project.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find ~/Workspace/Cats-and-Dogs-Classification/ -type d -name "*.pyc" -exec rm {} \;
-find ~/Workspace/Cats-and-Dogs-Classification/ -type d -name "*.DS_Store" -exec rm {} \;
-find ~/Workspace/Cats-and-Dogs-Classification/ -type d -name "*__pycache__" -exec rm -rf {} \;
